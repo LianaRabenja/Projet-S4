3142acd74d2d069818fc6ca7ff0e3e6eec88b15d
Liana Rabenja <119034420+LianaRabenja@users.noreply.github.com>
2023-07-10 21:23:18 +0300

    changement

--- Sakafo/application/views/Commencer.php
+++ Sakafo/application/views/Commencer.php
@@ -5,44 +5,57 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link rel="stylesheet" href="<?php echo base_url('assets/css/Style.css')?>">
+    <link rel="stylesheet" href="<?php echo base_url('assets/vendors/themify-icons/css/themify-icons.css')?>">
+
+<link rel="stylesheet" href="<?php echo base_url('assets/vendors/animate/animate.css')?>">
+
+<!-- Bootstrap + FoodHut main styles -->
+<link rel="stylesheet" href="<?php echo base_url('assets/css/foodhut.css')?> ">
 </head>
 <body>
 <!-- navbar -->
 <nav class="custom-navbar navbar navbar-expand-lg navbar-dark fixed-top" data-spy="affix" data-offset-top="10">
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav">
-                <li class="nav-item">
-                    <a class="nav-link" href="">Home</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="">Suggestion</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="<?php echo base_url("index.php/Go/Go")?>">Commencez</a>
-                </li>
-            </ul>
-            <a class="navbar-brand m-auto" href="#">
-                <img src="assets/imgs/logo.svg" class="brand-img" alt="">
-                <span class="brand-txt">Meal</span>
-            </a>
-        </div>
-    </nav>
-
-
-
-
-    <center>
-        <div class="radio-center">
-            <div class="centrer">
-                <br><br><p><input type="checkbox" name="" id="">Perdre poids</p>
-                <p><input type="checkbox" name="" id="">Augmenter de poids</p>
-                <input type="submit" value="Valider" style="background-color: crimson; border-radius: 5px;">    
-            </div>
-                  </div>
-    </center>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav">
+            <li class="nav-item">
+                <a class="nav-link" href="<?php echo base_url("index.php/Welcome/index")?>">Home</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#about">Suggestion</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="<?php echo base_url("index.php/Go/Go")?>">Commencer</a>
+            </li>
+        </ul>
+        <a class="navbar-brand m-auto" href="#">
+            <img src="<?php echo base_url('assets/imgs/logo.svg')?>" class="brand-img" alt="">
+            <span class="brand-txt">MEAL</span>
+        </a>
+        <ul class="navbar-nav">
+            <!-- <li class="nav-item">
+                <a class="nav-link" href="#blog">Blog<span class="sr-only">(current)</span></a>
+            </li> -->
+            
+        
+            <li class="nav-item">
+                <a href="components.html" class="btn btn-primary ml-xl-4">Deconnexio</a>
+            </li>
+        </ul>
+    </div>
+</nav>
+    
+    
+    
+    
+    
+    <div class="radio-center">
+        <br><br><p style="Font-size: 30px;"><input type="checkbox" name="" id="">Perdre poids</p>
+        <br><p style="Font-size: 30px;"><input type="checkbox" name="" id="">Augmenter de poids</p>
+        <br><input type="submit" value="Valider" style="background-color: crimson; border-radius: 5px;Font-size: 30px;">
+    </div>
     <script src="<?php echo base_url("assets/vendors/jquery/jquery-3.4.1.js")?>"></script>
     <script src="<?php echo base_url("assets/vendors/bootstrap/bootstrap.bundle.js") ?>"></script>
 
--- Sakafo/application/views/Home.php
+++ Sakafo/application/views/Home.php
@@ -6,39 +6,48 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 	<meta charset="utf-8">
 	<title>Welcome to CodeIgniter</title>
 	 <!-- font icons -->
-	 <link rel="stylesheet" href="<?php echo base_url('assets/vendors/themify-icons/css/themify-icons.css') ?>">
+     <link rel="stylesheet" href="<?php echo base_url('assets/vendors/themify-icons/css/themify-icons.css')?>">
 
-	<link rel="stylesheet" href="<?php echo base_url('assets/vendors/animate/animate.css')?>">
+<link rel="stylesheet" href="<?php echo base_url('assets/vendors/animate/animate.css')?>">
 
-	<!-- Bootstrap + FoodHut main styles -->
-	<link rel="stylesheet" href="<?php echo base_url('assets/css/foodhut.css')?>">
-
+<!-- Bootstrap + FoodHut main styles -->
+<link rel="stylesheet" href="<?php echo base_url('assets/css/foodhut.css')?> ">
 </head>
-<body>
-	<!-- navbar -->
-<nav class="custom-navbar navbar navbar-expand-lg navbar-dark fixed-top" data-spy="affix" data-offset-top="10">
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav">
-                <li class="nav-item">
-                    <a class="nav-link" href="">Home</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="">Suggestion</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="<?php echo base_url("index.php/Go/Go")?>">Commencez</a>
-                </li>
-            </ul>
-            <a class="navbar-brand m-auto" href="#">
-                <img src="assets/imgs/logo.svg" class="brand-img" alt="">
-                <span class="brand-txt">Meal</span>
-            </a>
-        </div>
-    </nav>
+<body data-spy="scroll" data-target=".navbar" data-offset="40" id="home">
 
+<!-- Navbar -->
+<nav class="custom-navbar navbar navbar-expand-lg navbar-dark fixed-top" data-spy="affix" data-offset-top="10">
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav">
+            <li class="nav-item">
+                <a class="nav-link" href="#home">Home</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#about">Suggestion</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="<?php echo base_url("index.php/Go/Go")?>">Commencer</a>
+            </li>
+        </ul>
+        <a class="navbar-brand m-auto" href="#">
+            <img src="<?php echo base_url('assets/imgs/logo.svg')?>" class="brand-img" alt="">
+            <span class="brand-txt">MEAL</span>
+        </a>
+        <ul class="navbar-nav">
+            <!-- <li class="nav-item">
+                <a class="nav-link" href="#blog">Blog<span class="sr-only">(current)</span></a>
+            </li> -->
+            
+        
+            <li class="nav-item">
+                <a href="components.html" class="btn btn-primary ml-xl-4">Deconnexio</a>
+            </li>
+        </ul>
+    </div>
+</nav>
 
 	<!-- blog -->
 	<br><div id="blog" class="container-fluid bg-dark text-light py-5 text-center wow fadeIn">
