8c59ef78c26c9e686b1b14b6a1652066797a02a1
Liana Rabenja <119034420+LianaRabenja@users.noreply.github.com>
2023-07-10 18:10:07 +0300

    push

--- Sakafo/application/views/Commencer.php
+++ Sakafo/application/views/Commencer.php
@@ -7,6 +7,33 @@
     <link rel="stylesheet" href="<?php echo base_url('assets/css/Style.css')?>">
 </head>
 <body>
+<!-- navbar -->
+<nav class="custom-navbar navbar navbar-expand-lg navbar-dark fixed-top" data-spy="affix" data-offset-top="10">
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link" href="">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="">Suggestion</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="<?php echo base_url("index.php/Go/Go")?>">Commencez</a>
+                </li>
+            </ul>
+            <a class="navbar-brand m-auto" href="#">
+                <img src="assets/imgs/logo.svg" class="brand-img" alt="">
+                <span class="brand-txt">Meal</span>
+            </a>
+        </div>
+    </nav>
+
+
+
+
     <center>
         <div class="radio-center">
             <div class="centrer">
@@ -16,6 +43,19 @@
             </div>
                   </div>
     </center>
+    <script src="<?php echo base_url("assets/vendors/jquery/jquery-3.4.1.js")?>"></script>
+    <script src="<?php echo base_url("assets/vendors/bootstrap/bootstrap.bundle.js") ?>"></script>
+
+    <!-- bootstrap affix -->
+    <script src="<?php echo base_url("assets/vendors/bootstrap/bootstrap.affix.js") ?>"></script>
+
+    <!-- wow.js -->
+    <script src="<?php echo base_url("assets/vendors/wow/wow.js") ?>"></script>
     
+    <!-- google maps -->
+    <script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCtme10pzgKSPeJVJrG1O3tjR6lk98o4w8&callback=initMap"></script>
+
+    <!-- FoodHut js -->
+    <script src="<?php echo base_url("assets/js/foodhut.js")?>"></script>
 </body>
 </html>
